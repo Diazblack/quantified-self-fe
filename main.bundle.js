424ce7ca3d8db0e66ebfb0e4addca920faa95064
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-30 16:20:07 -0700

    Add functionality to add a food to the database

--- main.bundle.js
+++ main.bundle.js
@@ -57,8 +57,32 @@
 	  });
 	}
 
+	function postFood() {
+	  var _this2 = this;
+
+	  var url = "https://fast-meadow-36413.herokuapp.com/api/v1/foods";
+	  var payload = {
+	    "food": {
+	      "name": document.getElementById('foodName').value,
+	      "calories": document.getElementById('foodCalorie').value
+	    }
+	  };
+
+	  fetch(url, {
+	    method: 'POST',
+	    headers: { 'Accept': 'application/json',
+	      'Content-Type': 'application/json' },
+	    body: JSON.stringify(payload)
+	  }).then(function (response) {
+	    return response.json();
+	  }).catch(function (error) {
+	    return console.error(error);
+	  }).then(function (json_response) {
+	    return _this2.ammendFoods(json_response);
+	  });
+	}
+
 	function showFoods(json_response) {
-	  debugger;
 	  var foodArray = json_response;
 	  var table = document.getElementById("foodsTable").getElementsByTagName('tbody')[0];
 
@@ -66,18 +90,35 @@
 	    var name = food['name'];
 	    var calories = food['calories'];
 	    var food_id = food['id'];
-	    food_id = "<button class='button' id='" + food_id + "' onclick=\"removeFood()\">-</button>";
+	    food_id = "<button class='delete-btn' id='" + food_id + "' onclick=\"removeFood()\">-</button>";
 	    var row = table.insertRow(0);
 	    var cell1 = row.insertCell(0);
 	    var cell2 = row.insertCell(1);
 	    var cell3 = row.insertCell(2);
-
+	    row.id = name;
 	    cell1.innerHTML = name;
 	    cell2.innerHTML = calories;
 	    cell3.innerHTML = food_id;
 	  });
 	}
 
+	function ammendFoods(json_response) {
+	  var food = json_response;
+	  var table = document.getElementById("foodsTable").getElementsByTagName('tbody')[0];
+
+	  var name = food['name'];
+	  var calories = food['calories'];
+	  var food_id = food['id'];
+	  food_id = "<button class='delete-btn' id='" + food_id + "' onclick=\"removeFood()\">-</button>";
+	  var row = table.insertRow(0);
+	  var cell1 = row.insertCell(0);
+	  var cell2 = row.insertCell(1);
+	  var cell3 = row.insertCell(2);
+	  row.id = name;
+	  cell1.innerHTML = name;
+	  cell2.innerHTML = calories;
+	  cell3.innerHTML = food_id;
+	}
 
 	function filterFoods() {
 	  var food = document.getElementById("filter").value;
@@ -98,7 +139,6 @@
 	}
 
 	$(window).load(getFoods());
-
 
 /***/ })
 /******/ ]);
