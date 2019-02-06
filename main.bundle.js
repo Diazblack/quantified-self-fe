44b201dfdacb0015fd211cf136bfd28ca4f9640e
Jenn Lao <jenn.p.lao@gmail.com>
2019-02-06 07:41:16 -0700

    Run build

--- main.bundle.js
+++ main.bundle.js
@@ -49,7 +49,7 @@
 	function getFoods() {
 	  var _this = this;
 
-	  var url = "https://fast-meadow-36413.herokuapp.com/api/v1/foods";
+	  var url = "https://quantified-self-533.herokuapp.com/api/v1/foods";
 	  fetch(url).then(function (response) {
 	    return response.json();
 	  }).then(function (json_response) {
@@ -60,14 +60,11 @@
 	function postFood() {
 	  var _this2 = this;
 
-	  var url = "https://fast-meadow-36413.herokuapp.com/api/v1/foods";
+	  var url = "https://quantified-self-533.herokuapp.com/api/v1/foods";
 	  var payload = {
-	    "food": {
-	      "name": document.getElementById('foodName').value,
-	      "calories": document.getElementById('foodCalorie').value
-	    }
+	    "name": document.getElementById('foodName').value,
+	    "calories": document.getElementById('foodCalorie').value
 	  };
-
 	  fetch(url, {
 	    method: 'POST',
 	    headers: { 'Accept': 'application/json',
@@ -82,30 +79,15 @@
 	  });
 	}
 
-	function editFood(id) {
-	  var food_id = id;
-	  var name_value = document.getElementById(food_id + "-name").innerHTML;
-	  var food_name_input = "<input id='edit-name' value=" + name_value + ">";
-	  var calories_value = document.getElementById(food_id + "-calories").innerHTML;
-	  var food_calories_input = "<input id='edit-calories' value=" + calories_value + ">";
-
-	  document.getElementById(food_id + "-edit").style.display = "none";
-	  document.getElementById(food_id + "-save").style.display = "block";
-	  document.getElementById(food_id + "-name").innerHTML = food_name_input;
-	  document.getElementById(food_id + "-calories").innerHTML = food_calories_input;
-	}
-
 	function saveFood(id) {
 	  var _this3 = this;
 
 	  var food_id = id;
 
-	  var url = "https://fast-meadow-36413.herokuapp.com/api/v1/foods/" + food_id;
+	  var url = "https://quantified-self-533.herokuapp.com/api/v1/foods/" + food_id;
 	  var payload = {
-	    "food": {
-	      "name": document.getElementById("edit-name").value,
-	      "calories": document.getElementById("edit-calories").value
-	    }
+	    "name": document.getElementById("edit-name").value,
+	    "calories": document.getElementById("edit-calories").value
 	  };
 
 	  fetch(url, {
@@ -122,12 +104,40 @@
 	  });
 	}
 
-	function patchFoods(json_response) {
-	  var food_id = json_response['id'];
-	  var updated_name = json_response['name'];
-	  var updated_calories = json_response['calories'];
+	function removeFood(id) {
+	  var _this4 = this;
 
-	  document.getElementById(food_id + "-edit").style.display = "block";
+	  var url = "https://quantified-self-533.herokuapp.com/api/v1/foods/" + id;
+	  fetch(url, {
+	    method: 'DELETE',
+	    headers: { 'Accept': 'application/json',
+	      'Content-Type': 'application/json' }
+	  }).then(function (response) {
+	    return _this4.getFoods();
+	  }).catch(function (error) {
+	    return console.error(error);
+	  });
+	}
+
+	function editFood(id) {
+	  var food_id = id;
+	  var name_value = document.getElementById(food_id + "-name").innerHTML;
+	  var food_name_input = "<input id='edit-name' value=" + name_value + ">";
+	  var calories_value = document.getElementById(food_id + "-calories").innerHTML;
+	  var food_calories_input = "<input id='edit-calories' value=" + calories_value + ">";
+
+	  document.getElementById(food_id + "-edit").style.display = "none";
+	  document.getElementById(food_id + "-save").style.display = "inline-block";
+	  document.getElementById(food_id + "-name").innerHTML = food_name_input;
+	  document.getElementById(food_id + "-calories").innerHTML = food_calories_input;
+	}
+
+	function patchFoods(json_response) {
+	  var food_id = json_response[0]['id'];
+	  var updated_name = json_response[0]['name'];
+	  var updated_calories = json_response[0]['calories'];
+
+	  document.getElementById(food_id + "-edit").style.display = "inline-block";
 	  document.getElementById(food_id + "-save").style.display = "none";
 
 	  document.getElementById(food_id + "-name").innerHTML = updated_name;
@@ -137,14 +147,15 @@
 	function showFoods(json_response) {
 	  var foodArray = json_response;
 	  var table = document.getElementById("foodsTable").getElementsByTagName('tbody')[0];
-
-	  foodArray.forEach(function (food) {
+	  while (table.firstChild) {
+	    table.removeChild(table.firstChild);
+	  }foodArray.forEach(function (food) {
 	    var name = food['name'];
 	    var calories = food['calories'];
 	    var food_id = food['id'];
 	    edit_food = "<button class='button' id=\"" + food_id + "-edit\" onclick=\"editFood(" + food_id + ")\">Edit</button>";
-	    save_food = "<button class='button' id=\"" + food_id + "-save\" onclick=\"saveFood(" + food_id + ")\" style=\"display: none;\">save</button>";
-	    delete_food = "<button class='delete-btn' id='" + food_id + "-del' onclick=\"removeFood()\">-</button>";
+	    save_food = "<button class='button' id=\"" + food_id + "-save\" onclick=\"saveFood(" + food_id + ")\" style=\"display: none;\">Save</button>";
+	    delete_food = "<button class='delete-btn' id='" + food_id + "-del' onclick=\"removeFood(" + food_id + ")\">-</button>";
 	    var row = table.insertRow(0);
 	    var cell1 = row.insertCell(0);
 	    var cell2 = row.insertCell(1);
@@ -167,7 +178,8 @@
 	  var calories = food['calories'];
 	  var food_id = food['id'];
 	  edit_food = "<button class='button' id='edit-" + food_id + "' onclick=\"editFood()\">Edit</button>";
-	  delete_food = "<button class='delete-btn' id='del-" + food_id + "' onclick=\"removeFood()\">-</button>";
+	  save_food = "<button class='button' id=\"" + food_id + "-save\" onclick=\"saveFood(" + food_id + ")\" style=\"display: none;\">Save</button>";
+	  delete_food = "<button class='delete-btn' id='del-" + food_id + "' onclick=\"removeFood(" + food_id + ")\">-</button>";
 	  var row = table.insertRow(0);
 	  var cell1 = row.insertCell(0);
 	  var cell2 = row.insertCell(1);
@@ -175,9 +187,9 @@
 	  var cell4 = row.insertCell(3);
 
 	  row.id = name;
-	  cell1.innerHTML = name;
-	  cell2.innerHTML = calories;
-	  cell3.innerHTML = edit_food;
+	  cell1.innerHTML = "<h4 id=\"" + food_id + "-name\">" + name + "</h4>";
+	  cell2.innerHTML = "<h4 id=\"" + food_id + "-calories\">" + calories + "</h4>";
+	  cell3.innerHTML = edit_food + save_food;
 	  cell4.innerHTML = delete_food;
 	}
 
